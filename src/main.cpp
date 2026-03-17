6a6f28ca6bd6ab9fd48a0198ee4785bcf2f7b3a1
huykhanh1602 <24020179@vnu.edu.vn>
2026-03-17 18:35:39 +0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,10 @@ int main() {
         std::getline(std::cin, command);
         if (command == "exit") {
             break;
-        } else if (command == "echo") {
-            std::cout << command << std::endl;
+        } else if (command.substr(0, 5) == "echo ") {
+            std::cout << command.substr(5) << std::endl;
+        } else {
+            std::cout << command << ": command not found" << std::endl;
         }
-        std::cout << command << ": command not found" << std::endl;
     }
 }
