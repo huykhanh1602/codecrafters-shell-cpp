df200f3a0fad3ff605801769ace01282933f3661
huykhanh1602 <24020179@vnu.edu.vn>
2026-03-20 16:59:31 +0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ int main() {
             break;
         } else if (command.substr(0, 5) == "echo ") {
             std::cout << command.substr(5) << std::endl;
+        } else if (command.substr(0, 5) == "type ") {
+            std::cout << command.substr(5) << " is a shell builtin" << std::endl;
         } else {
             std::cout << command << ": command not found" << std::endl;
         }
