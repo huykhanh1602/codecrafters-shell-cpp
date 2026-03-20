626c248e0942e3fb50aa919ac7e57171a2867d49
huykhanh1602 <24020179@vnu.edu.vn>
2026-03-20 17:11:46 +0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,24 @@ int main() {
         std::cout << "$ ";
         std::string command;
         std::getline(std::cin, command);
+
+        std::string builtin[] = {"echo", "type", "exit"};
         if (command == "exit") {
             break;
         } else if (command.substr(0, 5) == "echo ") {
             std::cout << command.substr(5) << std::endl;
         } else if (command.substr(0, 5) == "type ") {
-            std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+            bool found = false;
+            for (const auto &cmd : builtin) {
+                if (command.substr(5) == cmd) {
+                    std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                std::cout << command.substr(5) << ": not found" << std::endl;
+            }
         } else {
             std::cout << command << ": command not found" << std::endl;
         }
