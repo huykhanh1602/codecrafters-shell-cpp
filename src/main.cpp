23f80419e283ed6bc9f890f08c6a9c510f56301b
huykhanh1602 <huykhanhvu1602@gmail.com>
2026-06-03 22:11:55 +0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,19 @@ const char PATH_SEPARATOR = ':';
 namespace fs = std::filesystem;
 std::string builtin[] = {"echo", "type", "exit"};
 
+/**
+ * @brief Checks if a file is executable
+ * @param path The path to check
+ * @return The full path if the file is executable, otherwise an empty string
+ */
 std::string is_executable(const fs::path &path) {
+    // Get the PATH environment variable
     char *env_path = std::getenv("PATH");
     if (env_path == nullptr)
         return "";
-
     std::string_view path_env(env_path);
 
+    // Split the PATH variable and check each directory for the executable
     size_t start = 0;
     size_t end;
 #ifdef _WIN32
@@ -28,7 +34,7 @@ std::string is_executable(const fs::path &path) {
         return is_executable(path.string() + ".exe");
     }
 #endif
-
+    // Iterate through each directory in PATH
     while ((end = path_env.find(PATH_SEPARATOR, start)) != std::string_view::npos) {
         fs::path full_path = fs::path(path_env.substr(start, end - start)) / path;
         std::cout << "Checking " << full_path << std::endl;
@@ -40,14 +46,22 @@ std::string is_executable(const fs::path &path) {
     return "";
 }
 
+/**
+ * @brief Checks if a command is a builtin or an executable and prints its type
+ * @param input The command to check
+ */
 void type(std::string input) {
     bool found = false;
+
+    // check if the command is a builtin
     for (const auto &cmd : builtin) {
         if (input == cmd) {
             found = true;
             break;
         }
     }
+
+    // check if the command is an executable in PATH
     if (found) {
         std::cout << input << " is a shell builtin" << std::endl;
     } else if (const auto &exec_path = is_executable(input); !exec_path.empty()) {
@@ -76,6 +90,10 @@ int main() {
             std::cout << command.substr(5) << std::endl;
         } else if (command.substr(0, 5) == "type ") {
             type(command.substr(5));
+        } else if (command == "") {
+            // Do nothing for empty commands
+        } else if (std::string exec_path = is_executable(command); !exec_path.empty()) {
+            std::cout << command << " is " << exec_path << std::endl;
         } else {
             std::cout << command << ": command not found" << std::endl;
         }
